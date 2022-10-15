dd18fb1356ab94254ff3df41711884b432029c25
talhahafeez786 <112127141+talhahafeez786@users.noreply.github.com>
2022-10-15 19:54:39 +0500

    Update services.py

--- services.py
+++ services.py
@@ -3,3 +3,17 @@ import models as _models
 
 def _add_tables():
     return _database.Base.metadata.create_all(bind=_database.engine)
+
+def get_db():
+    db = _database.Session()
+    try:
+        yield db
+    finally: 
+        db.close()
+        
+async def create_contact(contact: _schemas.CreateContact, db: "Session") _schemas.Contact:
+    contact = _models.Contact(**contact.dict())
+    db.add(contact)
+    db.commit()
+    db.refresh(contact)
+    return _schemas.Contact.from_orm(contact)
