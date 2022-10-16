e5f1fc0e00595cf454aa0a229f47974fe6d8fbd7
talhahafeez786 <112127141+talhahafeez786@users.noreply.github.com>
2022-10-16 14:18:56 +0500

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ if TYPE_CHECKING:
 
 app = _fastapi.FastAPI()
 
-
-@app.post("/api/contact/", response_model= _schemas.Contact)
-async def create_contact(contact: _schemas.CreateContact, db: _orm.Session= _fastapi.Depends(_services.get_db)):
-    pass 
-
-
+@app.post("/api/contacts/", response_model=_schemas.Contact)
+async def create_contact(
+    contact: _schemas.CreateContact,
+    db: _orm.Session = _fastapi.Depends(_services.get_db),
+):
+    return await _services.create_contact(contact=contact, db=db)
